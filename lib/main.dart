3ab67b8e359f4f89976a026f4891a4b550db4843
Ahmet <ahmetc18@hotmail.com>
2022-05-28 17:28:08 +0300

    22

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gelisim_app/authentication/sign_in.dart';
 
+//deneme
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
