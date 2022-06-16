cc932a784e89747a3604dceca836012eb05f507a
Ahmet <ahmetc18@hotmail.com>
2022-06-16 23:45:32 +0300

    empty definition removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:gelisim_app/authentication/sign_in.dart';
 import 'package:gelisim_app/view/PrimaryPage.dart';
 
-//deneme
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
